9d96eab63fe2b4e9546046cf06af14aea6628201
T-Bone <tony.fast@gmail.com>
2017-06-24 18:24:00 +0000

    Convert posts and scripts automatically

--- whatever/dummy.py
+++ whatever/dummy.py
@@ -1,20 +1,25 @@
 # coding: utf-8
 
-
 # # <del>`fidget`</del> <small>a `pythonic` functional programming `object`</small>
 #
 # > ...posers juxtapose juxtapositions
 #
 # > composers compose compositions
 
-from fidget import pipes as a, maps; an = the = then = a
+from fidget import pipes as a, maps
+an = the = then = a
 from copy import copy
 import pandas, requests, toolz
 
-triangles = a.combinations(r=2).zip(list('ABC'))[maps.concat().list()].concatv([list('ABC')]).list()('DEF')
+triangles = a.combinations(r=2).zip(list('ABC'))[maps.concat().list()].concatv(
+    [list('ABC')]).list()('DEF')
 triangles
 
 positions = {
-    'A': [-1, 0], 'B': [0, 1], 'C': [1, 0],
-    'D': [-.5, .5], 'E': [0, 0], 'F': [.5, .5],
+    'A': [-1, 0],
+    'B': [0, 1],
+    'C': [1, 0],
+    'D': [-.5, .5],
+    'E': [0, 0],
+    'F': [.5, .5],
 }
